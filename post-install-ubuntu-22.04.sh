af596df1e9fe69226ddd28dfd0f6de5b5cc6f67c
Jackson Veroneze <jacksonvze@gmail.com>
2024-05-24 16:30:21 -0300

    Update post-install-ubuntu-22.04.sh

--- post-install-ubuntu-22.04.sh
+++ post-install-ubuntu-22.04.sh
@@ -59,6 +59,15 @@ yes | sudo DEBIAN_FRONTEND=noninteractive apt install wireshark
 sudo usermod -aG wireshark $USER
 
 # -----------------------------------------------------------------------
+# Install Grafana
+# -----------------------------------------------------------------------
+sudo gpg -k
+sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
+sudo apt-get update
+sudo apt-get install k6
+
+# -----------------------------------------------------------------------
 # Clean apt
 # -----------------------------------------------------------------------
 sudo sudo apt clean && sudo apt autoclean && sudo apt autoremove -y && \
