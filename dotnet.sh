15c27a7bc9581cdf565e156a3f8a71d114ec3709
Jackson Veroneze <jacksonvze@gmail.com>
2024-06-25 22:18:47 -0300

    Update dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -11,9 +11,17 @@ sudo apt update && \
   sudo apt install -y dotnet-sdk-6.0 dotnet-sdk-8.0
 
 # -----------------------------------------------------------------------
+# Monitoring
+# -----------------------------------------------------------------------
+dotnet tool install --global dotnet-trace
+dotnet tool install --global dotnet-counters
+dotnet tool install --global dotnet-monitor
+
+# -----------------------------------------------------------------------
 # AWS
 # -----------------------------------------------------------------------
 dotnet new install Amazon.Lambda.Templates
 
 dotnet tool install -g Amazon.Lambda.Tools
 dotnet tool update -g Amazon.Lambda.Tools
+
