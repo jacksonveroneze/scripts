5c3702927a7df226d142306236682e4bb9b47743
Jackson Veroneze <jacksonvze@gmail.com>
2024-06-25 22:19:33 -0300

    Update dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -3,10 +3,6 @@
 # -----------------------------------------------------------------------
 # Install .NET
 # -----------------------------------------------------------------------
-#wget "https://packages.microsoft.com/config/ubuntu/$(lsb_release -sr)/packages-microsoft-prod.deb" -O packages-microsoft-prod.deb
-#sudo dpkg -i packages-microsoft-prod.deb
-#rm packages-microsoft-prod.deb
-
 sudo apt update && \
   sudo apt install -y dotnet-sdk-6.0 dotnet-sdk-8.0
 
@@ -24,4 +20,3 @@ dotnet new install Amazon.Lambda.Templates
 
 dotnet tool install -g Amazon.Lambda.Tools
 dotnet tool update -g Amazon.Lambda.Tools
-
